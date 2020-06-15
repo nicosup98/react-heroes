ed38926800d53afe83b3130d4b191438275ae3d5
nicosup <damiconicola98@gmail.com>
2020-06-15 17:56:45 -0400

    firdt commit

---
+++ src/components/navBar.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const NavBar = ()=>(
+    <nav className="hero is-info">
+        <div className="container ml-2">
+            <h1 className="title">navbar</h1>
+        </div>
+    </nav>
+)
